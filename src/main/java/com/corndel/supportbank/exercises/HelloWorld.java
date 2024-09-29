e703b7edc528e9ac717b9fa28ee535fecde9b1e7
Simon Haines <nimon@hotmail.co.uk>
2024-09-30 00:54:31 +0100

    solves day 1

--- src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
+++ src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
@@ -1,8 +1,22 @@
 package com.corndel.supportbank.exercises;
 
-// import picocli.CommandLine;
-// import picocli.CommandLine.Command;
-// import picocli.CommandLine.Parameters;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
-public class HelloWorld {
+@Command(name = "hello", subcommands = { ColorCommand.class })
+public class HelloWorld implements Runnable {
+  @Parameters(index = "0", description = "The name of the person to greet", defaultValue = "World")
+  public String name;
+
+  @Override
+  public void run() {
+    String msg = String.format("Hello, %s!", name);
+    System.out.println(msg);
+  }
+
+  public static void main(String[] args) {
+    int exitCode = new CommandLine(new HelloWorld()).execute(args);
+    System.exit(exitCode);
+  }
 }
